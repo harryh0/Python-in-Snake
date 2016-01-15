a7467dfb14d7b53474dd0a9d245c280e36d1f710
harryh0 <harrysiulungho@gmail.com>
2016-01-14 16:11:32 -0800

    Update gridDemo.py
    Update to the demo. Stills runs fairly slowly. . .

--- import-Tkinter-import-random-root-Tkinter.Tk-rows-23-columns-43-piece_row-rows/2-piece_col-columns/gridDemo.py
+++ import-Tkinter-import-random-root-Tkinter.Tk-rows-23-columns-43-piece_row-rows/2-piece_col-columns/gridDemo.py
@@ -1,76 +1,77 @@
 import Tkinter
-import sys
 
-class gridGUIDemo:
-	board_dic = {}
+piece_row = [11]
+piece_col = [22]
 
-	def __init__(self, rows, columns): 
-		piece_row = 0
-		piece_col = 0
+rows = 23
+columns = 43
 
-		for row in range(0, rows):
-			for col in range(0, columns):
-				self.board_dic[row,col] = "0"
+board = [[0 for x in range(0, columns)] for y in range(0, rows)]
 
-	def pressedUp(event):
-		print("pressed up")
-		self.piece_col-=1
+for row in range(0, rows):
+	for col in range(0, columns):
+		board[row][col]="0"
 
-	def pressedDown(event):
-		print("pressed down")
-		self.piece_col+=1
+def pressedUp(event):
+	print("pressed up")
+	piece_col[0]-=1
 
-	def pressedLeft(event):
-		print("pressed left")
-		self.piece_row-=1
+def pressedDown(event):
+	print("pressed down")
+	piece_col[0]+=1
 
-	def pressedRight(event):
-		print("pressed right")
-		self.piece_row+=1
+def pressedLeft(event):
+	print("pressed left")
+	piece_row[0]-=1
 
-	def setDirection(master):
-		master.bind_all("<Up>", pressedUp)
-		master.bind_all("<Down>", pressedDown)
-		master.bind_all("<Left>", pressedLeft)
-		master.bind_all("<Right>", pressedRight)
+def pressedRight(event):
+	print("pressed right")
+	piece_row[0]+=1
 
-	def setPiece(row, column):
-		self.board_dic[row,column] = "~"
-		#board[row][column] = "~"
+def setDirection():
+	root.bind_all("<Up>", pressedUp)
+	root.bind_all("<Down>", pressedDown)
+	root.bind_all("<Left>", pressedLeft)
+	root.bind_all("<Right>", pressedRight)
 
-	def getPiece():
-		# to access row: getPiece()[:1]
-		# to access col: getPiece()[1:]
-		return self.board_dic.keys()[self.board_dic.values().index("~")]
-		
-	def displayBoard(master):
-		for k in self.board_dic:
-			if(self.board_dic[k] == "~"):
-				Tkinter.Label(master, bg="white", borderwidth=1, height=1,width=2).grid(row=k[:1], column=k[1:2])
+def setPiece(row, column):
+	board[row][column] = "~"
+"""
+def getPiece():
+	# to access row: getPiece()[:1]
+	# to access col: getPiece()[1:]
+	return board_dic.keys()[board_dic.values().index("~")]
+"""
+def displayBoard():
+	for r in board:
+		for c in board[r]:
+			if(board[r][c] == "~"):
+				Tkinter.Label(root, bg="white", borderwidth=1, height=1,width=2).grid(row=k[:1], column=k[1:])
 			else:
-				Tkinter.Label(master, bg="black", borderwidth=1, height=1,width=2).grid(row=k[:1],column=k[1:2])
+				Tkinter.Label(root, bg="black", borderwidth=1, height=1,width=2).grid(row=k[:1],column=k[1:])
+		
+def clearBoard():
+	for r in board:
+		for c in board[r]:
+			board[r][c] = "0"
 
-	def clearBoard():
-		for k in self.board_dic:
-			self.board_dic[k] = "0"
-
-	def updateBoard(master):
-		clearBoard()
-		setPiece(piece_row, piece_col)
-		displayBoard(master)
-		master.after(1000, updateBoard)
-
-def main(argv):
-	root = Tkinter.Tk()
-
-	grid = gridGUIDemo(23, 43)
-	setDirection(root)
-
-	setPiece(11, 22)
-	displayBoard(root)
-
+def updateBoard():
+	clearBoard()
+	setPiece(piece_row[0], piece_col[0])
+	displayBoard()
 	root.after(1000, updateBoard)
-	root.mainloop()
 
-if __name__ == "main":
-	main(sys.argv)
+
+root = Tkinter.Tk()
+
+setDirection()
+
+setPiece(piece_row[0], piece_col[0])
+displayBoard()
+
+root.after(1000, updateBoard)
+root.mainloop()
+
+# main issue: encounters lag when return
+# prognosis: Tkinter module too extensive, consider paring down only essential imports. 
+# for reference as main in snake game
